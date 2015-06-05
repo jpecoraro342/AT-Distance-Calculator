2702593f1b0596d20337f5fdb116fbb74165879f
jpecoraro342 <jpecoraro342@ufl.edu>
2015-06-04 23:25:24 -0400

    Updated entire app to use knockoutjs

--- js/scripts.js
+++ js/scripts.js
@@ -1,165 +1,143 @@
-//Locations
-var locationsCSV;
-var locations;
-var indexMap = {key: 'value'};
 
-var selected_start;
-var selected_end;
-
-//Document Setup/Prep
-function parseCSV(){
-	Papa.parse(locationsCSV, {
-      header: true,
-      dynamicTyping: true,
-      complete: function(results) {
-        locations = results.data;
-        addLocationsToList();
-      }
-    });
-}
-
-function addLocationsToList() {
-	var select1 = document.getElementById("startLocationList"); 
-	var select2 = document.getElementById("endLocationList"); 
-
-	for(var i = 0; i < locations.length; i++) {
-		var loc = locations[i];
-
-		var textvalue = loc.name + " - " + loc.distprev + " mi";
-		var key = loc.name;
-		key = key.replace(/\s/g,''); 
-		indexMap[key] = i;
-
-
-
-		var optionElement = document.createElement("li");
-		optionElement.appendChild(document.createTextNode(textvalue));
-		optionElement.setAttribute("class", "location col-xs-12");
-		optionElement.setAttribute("key", key);
-		optionElement.setAttribute("onclick", "newStartLocationSelected(this)")
-
-		var optionElement2 = document.createElement("li");
-		optionElement2.appendChild(document.createTextNode(textvalue));
-		optionElement2.setAttribute("class", "location col-xs-12");
-		optionElement2.setAttribute("key", key);
-		optionElement2.setAttribute("onclick", "newEndLocationSelected(this)")
-
-		select1.appendChild(optionElement);
-		select2.appendChild(optionElement2);
-	}
-}
-
-$(document).ready(function(){
+$(function(){
 	$.ajax({
             url: "./atdist.csv",
             async: false,
             success: function (data) {
-                locationsCSV = data;
-                parseCSV();
+                parseCSV(data);
             }
         });
 });
 
-//END Document Setup/Prep
-
-//ACTION
-function newStartLocationSelected(new_start_location) {
-	if (selected_start != null) {
-		selected_start.setAttribute("class", "location col-xs-12");
-	}
-	new_start_location.setAttribute("class", "location selected_loc col-xs-12");
-	selected_start = new_start_location;
-
-	UpdateInformation();
+function parseCSV(locationsCSV){
+	Papa.parse(locationsCSV, {
+      header: true,
+      dynamicTyping: true,
+      complete: function(results) {
+        setupViewModel(results.data);
+      }
+    });
 }
 
-function newEndLocationSelected(new_end_location) {
-	if (selected_end != null) {
-		selected_end.setAttribute("class", "location col-xs-12");
-	}
-	new_end_location.setAttribute("class", "location selected_loc col-xs-12");
-	selected_end = new_end_location;
+function setupViewModel(locations) {
+	var startLocations = [];
+	var endLocations = [];
 
-	UpdateInformation();
+	locations.forEach(function(locDict) {
+		startLocations.push(new Location(locDict));
+		endLocations.push(new Location(locDict));
+	});
+
+	var viewModel = new ATViewModel(startLocations, endLocations);
+
+	ko.applyBindings(viewModel);
 }
 
-//UI Updates
-function UpdateInformation() {
-	if (selected_start == null || selected_end == null) {
-		return;
-	}
-	var start_key = selected_start.getAttribute("key"); 
-	var end_key = selected_end.getAttribute("key");
+function Location(locationDict) {
+	this.name = locationDict.name;
+	this.springdist = locationDict.springdist;
+	this.distprev = locationDict.distprev;
+	this.elevation = locationDict.elevation;
+	this.elevationdiff = locationDict.elevationdiff;
+	this.steep = locationDict.steep;
+	this.amenities = locationDict.amenities;
 
-	var start_index = indexMap[start_key];
-	var end_index = indexMap[end_key];
-
-	var distance = locations[end_index].springdist - locations[start_index].springdist;
-	var elevationUp = getElevationUp(start_index, end_index);
-	var elevationDown = getElevationDown(start_index, end_index);
-	var amenities = locations[end_index].amenities;
-
-	var ul = document.getElementById("databox");
-	while (ul.firstChild) {
-	    ul.removeChild(ul.firstChild);
-	}
-
-	var li = document.createElement("li");
-	li.setAttribute("class", "noliststyle");
-
-	li.appendChild(document.createTextNode("Distance: " + Math.round( distance * 100 ) / 100 + " mi"));
-	li.appendChild(document.createElement("br"));
-	li.appendChild(document.createTextNode("Elevation Gain: " + Math.round( elevationUp * 100 ) / 100 + " ft"));
-	li.appendChild(document.createElement("br"));
-	li.appendChild(document.createTextNode("Elevation Loss: " + Math.round( elevationDown * 100 ) / 100 + " ft"));
-	li.appendChild(document.createElement("br"));
-	li.appendChild(document.createTextNode("Start Elevation: " + locations[start_index].elevation + "       End Elevation: " + locations[end_index].elevation));
-	li.appendChild(document.createElement("br"));
-	li.appendChild(document.createElement("br"));
-	li.appendChild(document.createTextNode("Amenities: " + amenities));
-	ul.appendChild(li);
-
-	updateChart(start_index, end_index);
+	this.isSelected = ko.observable(false);
 }
 
-function getElevationUp(start_index, end_index) {
+Location.prototype.setSelected = function() {
+	
+}
+
+function ATViewModel(startLocations, endLocations) {
+	this.startLocations = ko.observableArray(startLocations);
+	this.endLocations = ko.observableArray(endLocations);
+
+	this.selectedStartIndex = ko.observable(0);
+	this.selectedEndIndex = ko.observable(4);
+
+	this.selectedStartLocation = ko.observable(this.startLocations()[this.selectedStartIndex()]);
+	this.selectedEndLocation = ko.observable(this.endLocations()[this.selectedEndIndex()]);
+
+	this.setStartSelected(this.selectedStartLocation(), this.selectedStartIndex());
+	this.setEndSelected(this.selectedEndLocation(), this.selectedEndIndex());
+
+	this.startElevation = ko.computed(function() { return this.selectedStartLocation().elevation }, this);
+	this.endElevation = ko.computed(function() { return this.selectedEndLocation().elevation }, this);
+
+	this.amenities = ko.computed(function() { return this.selectedEndLocation().amenities }, this);
+
+	this.distance = ko.computed(function() {
+		var tempDistance = this.selectedEndLocation().springdist - this.selectedStartLocation().springdist;
+		return Math.round(tempDistance * 100) / 100;
+	}, this);
+
+	this.elevationGain = ko.computed(function() {
+		var tempGain = this.getElevationUp(this.selectedStartIndex(), this.selectedEndIndex());
+		return Math.round(tempGain * 100 ) / 100
+	}, this);
+
+	this.elevationLoss = ko.computed(function() {
+		var tempLoss = this.getElevationDown(this.selectedStartIndex(), this.selectedEndIndex());
+		return Math.round(tempLoss * 100 ) / 100
+	}, this);	
+
+	//TODO: Update Chart
+}
+
+ATViewModel.prototype.getElevationUp = function(start_index, end_index) {
 	var elevation = 0;
 	for (var i = start_index +1; i <= end_index; i++) {
-		temp_diff = locations[i].elevationdiff;
+		temp_diff = this.startLocations()[i].elevationdiff;
 		if (temp_diff > 0 ) {
 			elevation += temp_diff;
 		}
 	}
-
 	return elevation;
 }
 
-function getElevationDown(start_index, end_index) {
+ATViewModel.prototype.getElevationDown = function(start_index, end_index) {
 	var elevation = 0;
 	for (var i = start_index +1; i <= end_index; i++) {
-		temp_diff = locations[i].elevationdiff;
+		temp_diff = this.startLocations()[i].elevationdiff;
 		if (temp_diff < 0 ) {
 			elevation += temp_diff;
 		}
 	}
-
 	return elevation;
 }
 
-function updateChart(start_index, end_index){
+ATViewModel.prototype.setStartSelected = function(newStartLocation, newIndex) {
+	this.selectedStartIndex(newIndex);
+	this.selectedStartLocation().isSelected(false);
+	newStartLocation.isSelected(true);
+	this.selectedStartLocation(newStartLocation);
+
+	this.updateChart();
+}
+
+ATViewModel.prototype.setEndSelected = function(newEndLocation, newIndex) {
+	this.selectedEndIndex(newIndex);
+	this.selectedEndLocation().isSelected(false);
+	newEndLocation.isSelected(true);
+	this.selectedEndLocation(newEndLocation);
+
+	this.updateChart();
+}
+
+ATViewModel.prototype.updateChart = function() {
 	var dataArray = [];
 	var names = [];
 
-	var startLoc  = locations[start_index];
-	var startDist = startLoc.springdist;
+	var startDist = this.selectedStartLocation().springdist;
 
-	for (var i = start_index; i <= end_index; i++) {
-		var loc = locations[i];
+	for (var i = this.selectedStartIndex(); i <= this.selectedEndIndex(); i++) {
+		var loc = this.startLocations()[i];
 		var xvalue = loc.springdist - startDist;
 		var yvalue = loc.elevation;
 		var nameValue = loc.name;
-		dataArray[i - start_index] = [xvalue, yvalue];
-		names[i - start_index] = loc.name;
+		dataArray[i - this.selectedStartIndex()] = [xvalue, yvalue];
+		names[i - this.selectedStartIndex()] = loc.name;
 	}
 
 	$(function () {
